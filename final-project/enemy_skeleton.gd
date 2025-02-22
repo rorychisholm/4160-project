f7169f59ae063ba42f8b9acb6e3860eed1e8ae14
aidanMasonmondesire <aidanMasonmondesire@github.com>
2025-02-22 16:08:42 -0500

    enemy can die now

--- final-project/enemy_skeleton.gd
+++ final-project/enemy_skeleton.gd
@@ -4,10 +4,17 @@ extends CharacterBody2D
 @export var gravity = 980
 
 func _on_health_health_empty() -> void:
+	$AnimatedSprite2D.play("Death")
+	$AnimatedSprite2D.connect("animation_finished", Callable(self, "_on_death_animation_finished"))
+	
+func _on_death_animation_finished() -> void:
+	# Remove the character after the death animation finishes
 	queue_free()
 
 func _ready():
-	$AnimatedSprite2D.play()
+	$AnimatedSprite2D.flip_h = true
+	$AnimatedSprite2D.play("idle")
+	
 
 func _physics_process(delta):
 	# Apply gravity
